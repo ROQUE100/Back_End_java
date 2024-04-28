425ab278107af2d4ef2a7d5767394a9399ae960a
Claudio Roque <claudio_roque10@hotmail.com>
2024-04-28 19:04:10 -0300

    tipo de variaveis

--- tipos-de-variaveis/Tipos-Variaveis/src/TiposVariaveis.java
+++ tipos-de-variaveis/Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        double salarioMinimo = 2500; //esse valor é diferente de 2.500
+        double salarioMinimo2 = 2.500; //esse valor é = 2.5
+        short numeroCurto = 1;
+        int numeroNomal = 15000;       
+        
+       
+
+        final double VALOR_DE_PI = 3.14; //usamos "final" para informar que essa variavel nao muda de valor
+
+        System.out.println("o valorde pi é: " + VALOR_DE_PI);
+        
     }
 }
