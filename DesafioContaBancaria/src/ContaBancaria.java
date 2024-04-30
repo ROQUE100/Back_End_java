393e06bbf3b50f1ac679fcaa6d3536a4a8c0a808
Claudio Roque <claudio_roque10@hotmail.com>
2024-04-30 20:35:36 -0300

    desafio conta bancaria

---
+++ DesafioContaBancaria/src/ContaBancaria.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ContaBancaria {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem vindo ao Banco do Brasil, Por favor confirme seus dados: ");
+        System.out.println("seu nome: ");
+        String cliente = scanner.nextLine();       
+
+        System.out.println("sua agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("digite sua conta: 'sem digito' ");
+        int conta = scanner.nextInt();
+        
+
+        final double saldo = 700.37;
+
+        System.out.println("Obrigado " + cliente + " agencia: "+agencia + " conta: "+conta+" por confirmar seus dados, seu saldo é: "+ saldo +". deseja sacar alguma valor? ");
+
+        System.out.println("para sacar precione 'Y', para mostrar saldo precione 'N' ");
+        String sacar = "y";
+        String naoSacar = "n";        
+        String sacarValor = scanner.next();
+
+        if (naoSacar.equalsIgnoreCase(sacarValor)) {
+            System.out.println("seu saldo é: "+ saldo);            
+        }
+
+        if (sacar.equalsIgnoreCase(sacarValor)) {
+            System.out.println("Qual valor? ");
+            double valorSaque = scanner.nextDouble();
+            System.out.println(cliente+", saque realizado, seu novo saldo é: "+ (saldo - valorSaque)+ ". foi um prazer atender-lo(a)");
+        } else {
+            System.out.println("obrigado "+ cliente + ", foi um prazer atender-lo(a)");
+            
+        }
+
+        
+
+
+        
+
+
+
+        scanner.close();
+    }
+}
