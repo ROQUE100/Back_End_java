57e323a6b61653c2067ef88a48d2c80cc44dcdf9
Claudio Roque <claudio_roque10@hotmail.com>
2024-05-07 18:57:23 -0300

    refatorando o codigo selecao de candidatos para ficar mais legivel

--- ControleDeFluxo/src/ProcessoCeletivo.java
+++ ControleDeFluxo/src/ProcessoCeletivo.java
@@ -1,81 +1,78 @@
-// Importa as classes ArrayList e List do pacote java.util
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-// Declaração da classe ProcessoCeletivo
 public class ProcessoCeletivo {
-    // Método principal
     public static void main(String[] args) {
-        // Analisa os candidatos com seus respectivos salários pretendidos
-        analizarCandidato("claudio", 1900.0);
-        analizarCandidato("paulo", 2000.0);
-        analizarCandidato("maria",2200.0);       
-
-        // Chama o método de seleção de candidatos
-        selecaoCandidatos();
+        List<String> candidatosAprovados = selecaoCandidatos();
+        entrarEmContatoComCandidatos(candidatosAprovados);
     }    
 
-    // Método para selecionar os candidatos
-    static void selecaoCandidatos (){
-        // Array com os nomes dos candidatos
+    static List<String> selecaoCandidatos() {
         String [] candidatos = {"claudio", "maria", "julia", "pedro", "elias", "ana", "alex","joao", "joana"};
-
-        // Variáveis para controle do processo de seleção
         int candidatosSelecionados = 0;
-        int candidatoAtual =0;
         double salarioBase = 2000.0;
-
-        // Lista para armazenar os nomes dos candidatos aprovados
         List<String> candidatosAprovados = new ArrayList<>();
 
-        // Loop para selecionar os candidatos
-        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
-            // Obtém o nome e o salário pretendido do candidato atual
-            String candidato = candidatos[candidatoAtual];
+        for (String candidato : candidatos) {
             double salarioPretendido = valorPretendido();
-
-            // Exibe informações sobre o candidato e seu salário pretendido
-            System.out.println("O candidato "+ candidato + " solicitou este valor de salario "+ salarioPretendido);
-            // Verifica se o candidato foi selecionado com base no salário base
-            if (salarioBase >= salarioPretendido) {
+            System.out.println("O candidato "+ candidato + " solicitou este valor de salário "+ salarioPretendido);
+            
+            if (salarioBase >= salarioPretendido && candidatosSelecionados < 5) {
                 System.out.println("O candidato "+candidato+" foi selecionado para vaga");
-                // Adiciona o candidato à lista de aprovados
                 candidatosAprovados.add(candidato);
                 candidatosSelecionados++;
             } else {
                 System.out.println(candidato + " nao foi selecionado");
             }
-            candidatoAtual ++;            
-        }        
-        
-        // Imprime todos os candidatos inscritos
-        System.out.println("Este são todos os candidatos inscritos:");      
-        for(int indice=0; indice< candidatos.length; indice++){
-            System.out.println("o candidato de Nº " + (indice+1)+ " é o "+ candidatos[indice] );
         }
 
-        // Imprime a lista dos candidatos aprovados
-        System.out.println("Entre todos os escritos esses foram os aprovados: ");
-        for (int aprovados=0; aprovados<candidatosAprovados.size(); aprovados++){
+        System.out.println("Estes são todos os candidatos inscritos:");      
+        for (int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de Nº " + (indice+1)+ " é o "+ candidatos[indice] );
+        }
+
+        System.out.println("Dentre todos os inscritos, estes foram os aprovados: ");
+        for (int aprovados = 0; aprovados < candidatosAprovados.size(); aprovados++){
             System.out.println("Incrição: "+ (aprovados+1)+" Nome: " + candidatosAprovados.get(aprovados));
         }
+
+        return candidatosAprovados;
     }       
 
-    // Método para gerar valores aleatórios entre 1800 e 2200
-    static double valorPretendido(){
-        return ThreadLocalRandom.current().nextDouble(1800, 2200);
-    }
-    
-    // Método para analisar um candidato com base em seu nome e salário pretendido
-    static void analizarCandidato(String nome, double salarioPretendido){
-        double salarioBase = 2000.0;
-        if (salarioBase > salarioPretendido) {
-            System.out.println("ligar para o candidato "+nome+" para marcar entrevista");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("ligar para o candidato "+nome+" com contra proposta");            
-        } else{
-            System.out.println(nome + ", aguardando resultado das demais candidaturas");
+    static void entrarEmContatoComCandidatos(List<String> candidatosAprovados) {
+        for (String candidato : candidatosAprovados) {
+            entrarEmContato(candidato);
         }
     }
+
+    static void entrarEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Entrando em contato com o candidato: " + candidato);
+            }
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguimos contato com "+ candidato + " na "+ tentativasRealizadas);
+        } else {
+            System.out.println("Não conseguimos contato com "+ candidato + " após "+ tentativasRealizadas + " tentativas");
+        }
+    }
+
+    static boolean atender (){
+        return new Random().nextInt(3)==1;
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    } 
 }
