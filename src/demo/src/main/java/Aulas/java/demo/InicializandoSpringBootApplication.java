876e6ab79bcf6920d9e5bbf52913c1d6d280547f
Claudio Roque <claudio_roque10@hotmail.com>
2024-06-05 20:01:00 -0300

    entregando desafios

---
+++ src/demo/src/main/java/Aulas/java/demo/InicializandoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Aulas.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicializandoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicializandoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/demo/src/test/java/Aulas/java/demo/InicializandoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Aulas.java.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicializandoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
